fcc1cbfc063d0f92002666847c91bfb3a1b854e2
User <User@EPPLKRAW006A.budapest.epam.com>
2022-01-09 19:12:40 +0100

    small fixes in variable naming convention

--- src/test/java/com/epam/CarHuntingTest.java
+++ src/test/java/com/epam/CarHuntingTest.java
@@ -11,9 +11,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class CarHunting {
+public class CarHuntingTest {
 
-    @Test @DisplayName("Verify car with lowest price was parked")
+    @Test
+    @DisplayName("Verify car with lowest price was parked")
     public void carHunting() {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -38,16 +39,16 @@ public class CarHunting {
         //5. Sort results list by price ascending
         driver.findElement(By.xpath("//select[@id='so-sb']/option[@value='p-up']")).click();
         //6. Check that the results list is ordered accordingly
-        List<WebElement> prices = driver.findElements(By.xpath("//span[@class='h3 u-block']"));
-        int firstAdv = Integer.parseInt(prices.get(1).getText().replace(",", "").substring(1, 6));
-        int secondAdv = Integer.parseInt(prices.get(2).getText().replace(",", "").substring(1, 6));
-        int thirdAdv = Integer.parseInt(prices.get(3).getText().replace(",", "").substring(1, 6));
-        if (firstAdv <= secondAdv) {
-            boolean correctSort = secondAdv <= thirdAdv;
-            Assertions.assertTrue(correctSort);
+        List<WebElement> searchedResultCarsPrices = driver.findElements(By.xpath("//span[@class='h3 u-block']"));
+        int firstAdvertisedCarPrice = Integer.parseInt(searchedResultCarsPrices.get(1).getText().replace(",", "").substring(1, 6));
+        int secondAdvertisedCarPrice = Integer.parseInt(searchedResultCarsPrices.get(2).getText().replace(",", "").substring(1, 6));
+        int thirdAdvertisedCarPrice = Integer.parseInt(searchedResultCarsPrices.get(3).getText().replace(",", "").substring(1, 6));
+        if (firstAdvertisedCarPrice <= secondAdvertisedCarPrice) {
+            boolean priceLowestFirstSorting = secondAdvertisedCarPrice <= thirdAdvertisedCarPrice;
+            Assertions.assertTrue(priceLowestFirstSorting);
         }
         //7. Click on the item with the lowest price
-        prices.get(1).click();
+        searchedResultCarsPrices.get(1).click();
         //8. Verify the Technical specifications panel displayed
         driver.findElement(By.xpath("//div[@id='td-box']"));
         //9. "Park" the advertisement
@@ -57,7 +58,7 @@ public class CarHunting {
         driver.findElement(By.xpath("//a[@id='hdmysc2']")).click();
         //11. Verify that the saved advertisement is in the 'Car park'
         int priceParkedCar = Integer.parseInt(driver.findElement(By.xpath("//div[@class='parking__currentPrice']")).getText().replace(",", "").substring(1, 6));
-        Assertions.assertEquals(firstAdv, priceParkedCar);
+        Assertions.assertEquals(firstAdvertisedCarPrice, priceParkedCar);
 
         driver.quit();
     }
